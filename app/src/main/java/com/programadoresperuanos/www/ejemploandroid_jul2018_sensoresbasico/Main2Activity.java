38165584f803194146b85377d5f5d7c05f1c88e5
sud11real11 <real110686@gmail.com>
2018-07-23 21:49:26 -0500

    1

---
+++ app/src/main/java/com/programadoresperuanos/www/ejemploandroid_jul2018_sensoresbasico/Main2Activity.java
@@ -0,0 +1,39 @@
+package com.programadoresperuanos.www.ejemploandroid_jul2018_sensoresbasico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_uno,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.opcion1:
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                return true;
+            case R.id.opcion2:
+                startActivity(new Intent(getApplicationContext(),Main2Activity.class));
+                return true;
+            case R.id.opcion3:
+                startActivity(new Intent(getApplicationContext(),Main3Activity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
